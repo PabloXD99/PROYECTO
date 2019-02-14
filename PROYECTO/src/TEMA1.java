32d2001dcb38a7d6365f832b8f6f33b9298129ac
MyT25 <marco.antonio.noh1999@gmail.com>
2019-02-14 10:14:59 -0600

    ultima actualizacion

--- PROYECTO/src/TEMA1.java
+++ PROYECTO/src/TEMA1.java
@@ -1,6 +1,7 @@
 public class TEMA1 extends javax.swing.JFrame {
 
     /**
+     * AUTOR: noh noh marco antonio
      * Creates new form TEMA1
      */
     public TEMA1() {
@@ -376,8 +377,105 @@ public class TEMA1 extends javax.swing.JFrame {
 
     private void REDONDEOMouseClicked(java.awt.event.MouseEvent evt) {                                      
         // TODO add your handling code here:
-    }                                     
- // Variables declaration - do not modify                     
+    } 
+    
+    private void BUSCAR_VALORActionPerformed(java.awt.event.ActionEvent evt) {                                             
+       // realizar operaciones de truncamiento y redondeo
+       double ingreValor = Double.parseDouble(ingreso_de_valor.getText());
+       int deci = Integer.parseInt(INGRESAR_DECIMALES.getText());
+       
+       
+       if(TRUNCAMIENTO.isSelected()==true){
+           
+           
+           
+         ingreValor = Math.round(ingreValor * Math.pow(10,deci))/Math.pow(10,deci);
+           
+        
+           String tunc = String.valueOf(ingreValor);
+         TEXTO5.setText(tunc);
+       }
+       
+       if(REDONDEO.isSelected()==true){
+           
+       
+         
+          deci *= 100;
+          double iingreValor = Math.rint(ingreValor* deci)/deci;
+          String xnxx = String.valueOf(iingreValor);
+          TEXTO6.setText(xnxx);
+       }
+       
+       
+       
+        // TODO add your handling code here:
+    }                                            
+
+    private void ingreso_de_valorActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+        // TODO add your handling code here:
+    }                                                
+
+    private void INGRESAR_DECIMALESActionPerformed(java.awt.event.ActionEvent evt) {                                                   
+        // TODO add your handling code here:
+    }                                                  
+
+    private void TRUNCAMIENTOActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        // TODO add your handling code here:
+    }                                            
+
+    private void LIMPIAR_VALORESActionPerformed(java.awt.event.ActionEvent evt) {                                                
+TEXTO1.setText("");
+TEXTO2.setText("");
+TEXTO3.setText("");
+TEXTO4.setText("");
+TEXTO5.setText("");
+TEXTO6.setText("");
+INGRESAR_DECIMALES.setText("");
+ingreso_de_valor.setText("");
+TRUNCAMIENTO.setSelected(false) ;
+REDONDEO.setSelected(false);
+        
+
+
+// TODO add your handling code here:
+    }                                               
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TEMA1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TEMA1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TEMA1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TEMA1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TEMA1().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify                     
     private javax.swing.JButton BUSCAR_VALOR;
     private javax.swing.JButton CALCULAR_ERRORES;
     private javax.swing.JLabel EAR;
